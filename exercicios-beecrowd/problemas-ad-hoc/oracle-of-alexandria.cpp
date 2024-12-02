0bf95defd9f343c7349d3e86ca461ef0eb131764
Gabriel Teixeira Silveira <gabriel.silveira.dev@gmail.com>
2024-12-02 20:06:06 -0300

    Iniciei os exercícios ad hoc
    On branch main
    	new file:   exercicios-beecrowd/problemas-ad-hoc/oracle-of-alexandria.cpp

---
+++ exercicios-beecrowd/problemas-ad-hoc/oracle-of-alexandria.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+ 
+    int n;
+    int k;
+
+    char input;
+
+    cin >> input;
+    cout << input;
+
+
+ 
+    return 0;
+}
