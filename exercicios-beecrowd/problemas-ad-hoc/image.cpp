fe18ee1b6f64d34975f519e72da68f3d67d4f05d
Gabriel Teixeira Silveira <gabriel.silveira.dev@gmail.com>
2024-12-05 19:48:21 -0300

    Iniciei a Imagem 	new file:   image.cpp

---
+++ exercicios-beecrowd/problemas-ad-hoc/image.cpp
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <string>
+#include <sstream>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    int m;
+    vector<string> imagem;
+    
+    do
+    {
+        cin >> n >> m;
+
+        cin >> imagem[0];
+
+        cout << m << endl;
+        cout <<;
+    } while (n != 0 && m != 0);
+
+    cout << m << endl;
+    return 0;
+}
+/*
+n m
+3 3
+###
+#__
+###
+6 9
+0 0
+
+#########
+#########
+###______
+###______
+#########
+#########
+*/
