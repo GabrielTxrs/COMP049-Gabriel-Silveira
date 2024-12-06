e8fc3a668ed5aae853840c256b2bc7f3a69273cc
Gabriel Teixeira Silveira <gabriel.silveira.dev@gmail.com>
2024-12-05 22:23:08 -0300

    Iniciei a implementacao de Image.cpp new file:   ../../.vscode/c_cpp_properties.json modified:   ../../.vscode/settings.json modified:   image.cpp new file:   input_image.txt new file:   input_image_2.txt new file:   template.cpp

--- exercicios-beecrowd/problemas-ad-hoc/image.cpp
+++ exercicios-beecrowd/problemas-ad-hoc/image.cpp
@@ -1,28 +1,79 @@
-#include <stdio.h>
-#include <string>
-#include <sstream>
+#include <bits/stdc++.h>
 #include <iostream>
-#include <vector>
-
 using namespace std;
+typedef long long ll;
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3fll;
+#define _                         \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);
+#define dbg(i) cout << #i << " " << i << endl;
+#define forn(n, x)                   \
+    cout << "[";                     \
+    for (auto i = 0; i < n - 1; i++) \
+    {                                \
+        cout << x[i] << ", ";        \
+    }                                \
+    cout << x[n - 1] << "]" << endl;
+
+string resizeString(int n, const string &str)
+{
+    string result;
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result += str[i];
+        }
+    }
+    return result;
+}
 
 int main()
 {
     int n;
     int m;
+    int a;
+    int b;
+    int numLinhaResized;
+    int numColunaResized;
     vector<string> imagem;
-    
-    do
+    vector<string> imagemResized;
+
+    string linha;
+
+    while (true)
     {
         cin >> n >> m;
+        if (n == 0 && m == 0)
+            return 0;
 
-        cin >> imagem[0];
+        for (int i = 0; i < n; i++)
+        {
+            cin >> linha;
+            imagem.push_back(linha);
+        }
+        cin >> a >> b;
+        numLinhaResized = a / n;
+        numColunaResized = b / m;
 
-        cout << m << endl;
-        cout <<;
-    } while (n != 0 && m != 0);
+        for (size_t i = 0; i < numLinhaResized; i++)
+        {
+            for (size_t j = 0; j < numColunaResized; j++)
+            {
+                imagemResized.push_back(resizeString(numColunaResized, imagem[i]));
+            }
+        }
+        for (const auto &str : imagemResized)
+        {
+            cout << str << "\n";
+        }
+        cout << endl;
+        dbg(a);
+        dbg(b);
+    }
 
-    cout << m << endl;
     return 0;
 }
 /*
