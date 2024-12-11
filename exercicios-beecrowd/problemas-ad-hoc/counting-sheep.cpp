01e27f916309402b3ea61fa0a0c98501d5cb235a
Gabriel Teixeira Silveira <gabriel.silveira.dev@gmail.com>
2024-12-11 14:58:51 -0300

    Finalizei o problema Counting Sheep modified:   exercicios-beecrowd/problemas-ad-hoc/counting-sheep.cpp deleted:    exercicios-beecrowd/problemas-ad-hoc/input_sheep.txt

--- exercicios-beecrowd/problemas-ad-hoc/counting-sheep.cpp
+++ exercicios-beecrowd/problemas-ad-hoc/counting-sheep.cpp
@@ -7,22 +7,25 @@ const ll LINF = 0x3f3f3f3f3f3f3f3fll;
 #define _                         \
     ios_base::sync_with_stdio(0); \
     cin.tie(0);
-#define dbg(i) cout << #i << " " << i << endl;
-#define forn(n, x)                   \
-    cout << "[";                     \
-    for (auto i = 0; i < n - 1; i++) \
-    {                                \
-        cout << x[i] << ", ";        \
-    }                                \
-    cout << x[n - 1] << "]" << endl;
 
 int main()
 {
-    while (true)
+    _ ll cont_externo;
+    cin >> cont_externo;
+    for (size_t i = 0; i < cont_externo; i++)
     {
-        _ ll n;
-        cin >> n;
+        _ ll num_ovelhas;
+        cin >> num_ovelhas;
+        set<ll> ovelhas;
+        ll contadorDeOvelhas = 0;
+        for (size_t i = 0; i < num_ovelhas; i++)
+        {
+            ll ovelha;
+            cin >> ovelha;
+            if(ovelhas.insert(ovelha).second)
+                contadorDeOvelhas++;
+        }
+        cout << contadorDeOvelhas << endl;
     }
-
     return 0;
 }
